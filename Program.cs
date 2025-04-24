cc6a1211b67016297d5c838c04212ebe672512a6
Avi Chhetri <Avi Chhetri@DESKTOP-1KGM680>
2025-04-24 19:50:47 -0400

    coding practice

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codingtest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("reverse number " +
+                "of {0} is {1}", 123, Reverse.intReverse(123));
+
+
+            Console.WriteLine("reverse string of {0} is {1}", "Apple", Reverse.strReverse("Apple"));
+
+
+            Console.WriteLine("string is palindrome {0} is {1}", "dad", Reverse.strReverse("dad"));
+
+            Console.WriteLine("string is palindrome {0} is {1}", "Apple", Reverse.strPalindrome("Apple"));
+
+        }
+    }
+}
---
+++ Reverse.cs
@@ -0,0 +1,38 @@
+﻿namespace codingtest;
+
+public static class Reverse
+{
+    public static int intReverse(int num)
+    {
+        int revNum = 0;
+        do
+        {
+            int n = num % 10;
+            revNum = revNum* 10 + n;
+            num /= 10;
+        } while (num > 0);
+        return revNum;
+    }
+
+    public static string strReverse(string str)
+    {
+        char[] chars = str.ToCharArray();
+        string newString = "";
+        for (int i= chars.Length -1 ; i >=0 ; i--)
+        {
+            newString += chars[i].ToString();
+        }
+        return newString;
+    }
+
+    public static bool strPalindrome(string str)
+    {
+        char[] chars = str.ToCharArray();
+        string newString = "";
+        for (int i = chars.Length - 1; i >= 0; i--)
+        {
+            newString += chars[i].ToString();
+        }
+        return newString == str;
+    }
+}
